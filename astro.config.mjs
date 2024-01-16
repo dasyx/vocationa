7e74154621779131fcb64b490d4b885e94865a78
unknown <seanpnn00@gmail.com>
2024-01-17 00:04:36 +0100

    upgrade astro v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import tailwind from '@astrojs/tailwind'
 import svelte from '@astrojs/svelte'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import vercel from '@astrojs/vercel/serverless'
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://astroship.web3templates.com',
 	integrations: [tailwind(), svelte(), mdx(), sitemap()],
 	output: 'server',
-	adapter: vercel()
+	adapter: netlify()
 })
